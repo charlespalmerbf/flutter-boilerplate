0d9db03f855c8111eac9ede961e4c4c44f975602
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-04-25 17:55:50 +0100

    chore: refactoring shop page to use new drink component

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,4 +1,8 @@
+import 'package:bubble_tea/components/drink_tile.dart';
+import 'package:bubble_tea/models/drink.dart';
+import 'package:bubble_tea/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatefulWidget {
   const ShopPage({super.key});
@@ -10,14 +14,21 @@ class ShopPage extends StatefulWidget {
 class _ShopPageState extends State<ShopPage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(25.0),
-        child: Column(children: [
-          Text("Bubble Tea Shop", style: TextStyle(fontSize: 20)),
-          
-        ],),
-      ),
-    )
+    return Consumer<BubbleTeaShop>(
+        builder: (context, value, child) => SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(25.0),
+                child: Column(children: [
+                  const Text("Bubble Tea Shop", style: TextStyle(fontSize: 20)),
+                  Expanded(
+                      child: ListView.builder(
+                          itemCount: value.shop.length,
+                          itemBuilder: (context, index) {
+                            Drink individualDrink = value.shop[index];
+                            return DrinkTile(drink: individualDrink, onTap: () => goToOrder);
+                          }))
+                ]),
+              ),
+            ));
   }
 }
