65d945e89a79b3098c45c5cc035e9c43bd361e7d
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-04-25 16:54:02 +0100

    feat: adding shop page

---
+++ lib/pages/shop_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ShopPage extends StatefulWidget {
+  const ShopPage({super.key});
+
+  @override
+  State<ShopPage> createState() => _ShopPageState();
+}
+
+class _ShopPageState extends State<ShopPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("SHOP"));
+  }
+}
