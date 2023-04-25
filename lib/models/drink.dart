8ba0fab9eb187198865e93833965b7aca82df408
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-04-25 17:27:08 +0100

    feat: adding data models

---
+++ lib/models/drink.dart
@@ -0,0 +1,11 @@
+class Drink {
+  final String name;
+  final String price;
+  final String imagePath;
+
+  Drink({
+    required this.name,
+    required this.price,
+    required this.imagePath
+  })
+}
