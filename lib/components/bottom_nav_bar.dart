a9ce276fe19b8282e4abf97e849d1b6c1e81029e
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-04-25 16:54:47 +0100

    feat: adding bottom navigation bar component

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
+
+class BottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  BottomNavBar({super.key, required this.onTabChange});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 25.0),
+      child: GNav(
+        onTabChange:(value) => onTabChange!(value),
+        mainAxisAlignment: MainAxisAlignment.center,
+        activeColor: Colors.white,
+        color: Colors.grey[300],
+        tabActiveBorder: Border.all(color: Colors.white),
+        gap: 8,
+        tabs: const [
+          GButton(icon: Icons.home, text: 'Shop'),
+          GButton(icon: Icons.home, text: 'Shop')
+      ]),
+    );
+  }
+}
