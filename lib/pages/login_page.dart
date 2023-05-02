2605f355d817ad320862e3fbf94dd21981e30c15
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 22:43:23 +0100

    feat: adding splitter bar for sso options

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -51,6 +51,32 @@ class LoginPage extends StatelessWidget {
 
             const SizedBox(height: 50),
 
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    )
+                  ),
+            
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                    child: Text('Or continue with', style: TextStyle(color: Colors.grey[700]),),
+                  ),
+            
+                  Expanded(
+                    child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    )
+                  )
+                ],
+              ),
+            )
+
           ]),
         ),
       ),
