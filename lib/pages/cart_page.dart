2104e859e4311a9bd17056a5108c7e060c9c3ec7
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 15:41:00 +0100

    feat: resolving syntax issues and adding remove from cart method using shop provider

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,8 @@
+import 'package:bubble_tea/components/drink_tile.dart';
+import 'package:bubble_tea/models/drink.dart';
+import 'package:bubble_tea/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class CartPage extends StatefulWidget {
   const CartPage({super.key});
@@ -8,8 +12,24 @@ class CartPage extends StatefulWidget {
 }
 
 class _CartPageState extends State<CartPage> {
+
+  void removeFromCart(Drink drink) {
+    Provider.of<BubbleTeaShop>(context, listen: false).removeFromCart(drink);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("CART"));
+    return Consumer<BubbleTeaShop>(builder: (context, value, child) => SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.all(25.0),
+        child: Column(children: [
+          const Text('Your Cart', style: TextStyle(fontSize: 20)),
+          Expanded(child: ListView.builder(itemCount: value.cart.length, itemBuilder: (context, index) {
+            Drink drink = value.cart[index];
+            return DrinkTile(drink: drink, onTap: () => removeFromCart(drink), trailing: const Icon(Icons.delete));
+          })
+      )]),
+      ),
+    ));
   }
 }
