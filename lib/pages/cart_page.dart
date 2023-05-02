358116f117975b24f7f2994f3a8e660a71f78ef0
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 16:57:11 +0100

    feat: adding pay now button

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -19,16 +19,25 @@ class _CartPageState extends State<CartPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<BubbleTeaShop>(builder: (context, value, child) => SafeArea(
+    return Consumer<BubbleTeaShop>(builder: (context, value, child) =>    
+    SafeArea(
       child: Padding(
         padding: const EdgeInsets.all(25.0),
         child: Column(children: [
           const Text('Your Cart', style: TextStyle(fontSize: 20)),
+          const SizedBox(height: 10),
           Expanded(child: ListView.builder(itemCount: value.cart.length, itemBuilder: (context, index) {
             Drink drink = value.cart[index];
             return DrinkTile(drink: drink, onTap: () => removeFromCart(drink), trailing: const Icon(Icons.delete));
-          })
-      )]),
+          })),
+
+          MaterialButton(
+            color: Colors.brown,
+            onPressed: () {},
+            child: const Text('PAY', style: TextStyle(color: Colors.white),),
+          )
+
+        ]),
       ),
     ));
   }
