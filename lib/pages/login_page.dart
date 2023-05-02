787c5cf38386e07044735c80fabdd38cc163728c
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 22:43:56 +0100

    chore: format page

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -18,65 +18,58 @@ class LoginPage extends StatelessWidget {
         child: Center(
           child: Column(children: [
             const SizedBox(height: 50),
-
             const Icon(Icons.lock, size: 100),
-
             const SizedBox(height: 50),
-
-            Text('Welcome back, you\'ve been missed!', style: TextStyle(color: Colors.grey[700], fontSize: 16)),
-
+            Text('Welcome back, you\'ve been missed!',
+                style: TextStyle(color: Colors.grey[700], fontSize: 16)),
             const SizedBox(height: 25),
-
-            TextInput(controller: usernameController, hintText: 'Username', obscureText: false),
-
+            TextInput(
+                controller: usernameController,
+                hintText: 'Username',
+                obscureText: false),
             const SizedBox(height: 10),
-
-            TextInput(controller: passwordController, hintText: 'Password', obscureText: true),
-
+            TextInput(
+                controller: passwordController,
+                hintText: 'Password',
+                obscureText: true),
             const SizedBox(height: 10),
-
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: [
-                  Text('Forgot Password?', style: TextStyle(color: Colors.grey[600])),
+                  Text('Forgot Password?',
+                      style: TextStyle(color: Colors.grey[600])),
                 ],
               ),
             ),
-
             const SizedBox(height: 25),
-
             PrimaryButton(onTap: signUserIn),
-
             const SizedBox(height: 50),
-
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 25.0),
               child: Row(
                 children: [
                   Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey[400],
-                    )
-                  ),
-            
+                      child: Divider(
+                    thickness: 0.5,
+                    color: Colors.grey[400],
+                  )),
                   Padding(
                     padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text('Or continue with', style: TextStyle(color: Colors.grey[700]),),
+                    child: Text(
+                      'Or continue with',
+                      style: TextStyle(color: Colors.grey[700]),
+                    ),
                   ),
-            
                   Expanded(
-                    child: Divider(
-                      thickness: 0.5,
-                      color: Colors.grey[400],
-                    )
-                  )
+                      child: Divider(
+                    thickness: 0.5,
+                    color: Colors.grey[400],
+                  ))
                 ],
               ),
             )
-
           ]),
         ),
       ),
