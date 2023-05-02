0b011c76f65fa7391060f8a93e2ef643e459e48d
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 13:02:01 +0100

    feat: adding sliders

--- lib/pages/order_page.dart
+++ lib/pages/order_page.dart
@@ -11,13 +11,63 @@ class OrderPage extends StatefulWidget {
 }
 
 class _OrderPageState extends State<OrderPage> {
+  double sweetValue = 0.5;
+  double iceValue = 0.5;
+  double pearlValue = 0.5;
+
+  void customizeSweet(double newValue) {
+    setState(() {
+      sweetValue = newValue;
+    });
+  }
+
+  void customizeIce(double newValue) {
+    setState(() {
+      iceValue = newValue;
+    });
+  }
+
+  void customizePearl(double newValue) {
+    setState(() {
+      pearlValue = newValue;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.drink.name)),
       backgroundColor: Colors.brown[200],
       body: Column(children: [
-        Image.asset(widget.drink.imagePath)
+        Image.asset(widget.drink.imagePath),
+        Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                const Text('Sweetness'),
+                Expanded(child: Slider(value: sweetValue, label: sweetValue.toString(), divisions: 4, onChanged: (value) => customizeSweet(value)))
+              ],),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                const Text('Ice'),
+                Expanded(child: Slider(value: iceValue, label: iceValue.toString(), divisions: 4, onChanged: (value) => customizeIce(value)))
+              ],),
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+                const Text('Pearls'),
+                Expanded(child: Slider(value: pearlValue, label: pearlValue.toString(), divisions: 4, onChanged: (value) => customizePearl(value)))
+              ],),
+            )
+          ],
+        )
+
       ]),
     );
   }
