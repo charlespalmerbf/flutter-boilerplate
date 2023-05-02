ad0e28eaf76ab03d516fa1dde54f06592acfdcef
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 22:12:00 +0100

    feat: adding forgot password prompt text

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -29,6 +29,23 @@ class LoginPage extends StatelessWidget {
             const SizedBox(height: 10),
 
             TextInput(controller: passwordController, hintText: 'Password', obscureText: true),
+
+            const SizedBox(height: 10),
+
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text('Forgot Password?', style: TextStyle(color: Colors.grey[600])),
+                ],
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+
+            
           ]),
         ),
       ),
