38f2b333ca4b26aa8c86460439e6ddd952ba1939
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 23:03:33 +0100

    chore: styling sso tile component

--- lib/components/sso_tile.dart
+++ lib/components/sso_tile.dart
@@ -7,6 +7,10 @@ class SSOTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(child: Image.asset(imagePath));
+    return Container(
+      padding: const EdgeInsets.all(20),
+      decoration: BoxDecoration(border: Border.all(color: Colors.white), borderRadius: BorderRadius.circular(16), color: Colors.grey[200]),
+      child: Image.asset(imagePath)
+    );
   }
 }
