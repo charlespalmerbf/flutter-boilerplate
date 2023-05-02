95fb8a076c1c1e52482e9e255818dfaf793935aa
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-02 22:30:12 +0100

    feat: adding onTap functionality to primary button

--- lib/components/primary_button.dart
+++ lib/components/primary_button.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 
 class PrimaryButton extends StatelessWidget {
-  const PrimaryButton({super.key});
+
+  final Function()? onTap;
+
+  const PrimaryButton({super.key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(25),
-      margin: const EdgeInsets.symmetric(horizontal: 25),
-      decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8)),
-      child: const Center(child: Text('Sign In', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize:  16))),
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        padding: const EdgeInsets.all(25),
+        margin: const EdgeInsets.symmetric(horizontal: 25),
+        decoration: BoxDecoration(color: Colors.black, borderRadius: BorderRadius.circular(8)),
+        child: const Center(child: Text('Sign In', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize:  16))),
+      ),
     );
   }
 }
