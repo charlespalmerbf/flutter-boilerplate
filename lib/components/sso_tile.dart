0d488c8dc83f66f56f470f3c70f49b7f79d17aae
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 16:35:35 +0100

    chore: adding ontap function to sso tile

--- lib/components/sso_tile.dart
+++ lib/components/sso_tile.dart
@@ -2,20 +2,24 @@ import 'package:flutter/material.dart';
 
 class SSOTile extends StatelessWidget {
   final String imagePath;
+  final Function()? onTap;
 
-  const SSOTile({super.key, required this.imagePath});
+  const SSOTile({super.key, required this.imagePath, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(20),
-        decoration: BoxDecoration(
-            border: Border.all(color: Colors.white),
-            borderRadius: BorderRadius.circular(16),
-            color: Colors.grey[200]),
-        child: Image.asset(
-          imagePath,
-          height: 75,
-        ));
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+              borderRadius: BorderRadius.circular(16),
+              color: Colors.grey[200]),
+          child: Image.asset(
+            imagePath,
+            height: 75,
+          )),
+    );
   }
 }
