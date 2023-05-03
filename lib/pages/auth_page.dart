9c348c3147afce0e0a3bf95a4dfb832497b8fe19
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 15:00:07 +0100

    chore: pointing auth page at login and register toggle

--- lib/pages/auth_page.dart
+++ lib/pages/auth_page.dart
@@ -1,5 +1,5 @@
 import 'package:bubble_tea/pages/home_page.dart';
-import 'package:bubble_tea/pages/login_page.dart';
+import 'package:bubble_tea/pages/login_or_register_page.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -15,7 +15,7 @@ class AuthPage extends StatelessWidget {
           if (snapshot.hasData) {
             return HomePage();
           } else {
-            return LoginPage();
+            return const LoginOrRegisterPage();
           }
         },
       ),
