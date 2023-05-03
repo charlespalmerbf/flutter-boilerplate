c628f728f75d1ab1fd633d5792bed5bc60174076
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 09:44:21 +0100

    fix: adding height to images

--- lib/components/sso_tile.dart
+++ lib/components/sso_tile.dart
@@ -10,7 +10,7 @@ class SSOTile extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(border: Border.all(color: Colors.white), borderRadius: BorderRadius.circular(16), color: Colors.grey[200]),
-      child: Image.asset(imagePath)
+      child: Image.asset(imagePath, height: 75,)
     );
   }
 }
