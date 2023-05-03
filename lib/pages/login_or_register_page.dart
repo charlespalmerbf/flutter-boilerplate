6888cb31eee4f26b8c8d33a3f13a9004fb5dde2b
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 14:59:17 +0100

    feat: adding ontap and gesture detector to register now prompt

---
+++ lib/pages/login_or_register_page.dart
@@ -0,0 +1,28 @@
+import 'package:bubble_tea/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginOrRegisterPage extends StatefulWidget {
+  const LoginOrRegisterPage({super.key});
+
+  @override
+  State<LoginOrRegisterPage> createState() => _LoginOrRegisterPageState();
+}
+
+class _LoginOrRegisterPageState extends State<LoginOrRegisterPage> {
+  bool showLoginPage = true;
+
+  void togglePages() {
+    setState(() {
+      showLoginPage = !showLoginPage;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (showLoginPage) {
+      return LoginPage(onTap: togglePages);
+    } else {
+      return RegisterPage();
+    }
+  }
+}
