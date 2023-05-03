5e6cff6bf7ac53898f4d7fdd2032b05b5c3c7072
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 11:18:26 +0100

    feat: implementing auth page to check login status

---
+++ lib/pages/auth_page.dart
@@ -0,0 +1,24 @@
+import 'package:bubble_tea/pages/home_page.dart';
+import 'package:bubble_tea/pages/login_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return const HomePage();
+          } else {
+            return LoginPage();
+          }
+        },
+      ),
+    );
+  }
+}
