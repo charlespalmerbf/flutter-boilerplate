9012f255dbee4bea41d938d2e254ab16b269b488
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 09:44:57 +0100

    feat: adding single child scroll view to prevent overflow error

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -16,83 +16,85 @@ class LoginPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.grey[300],
       body: SafeArea(
-        child: Center(
-          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-            const SizedBox(height: 50),
-            const Icon(Icons.lock, size: 100),
-            const SizedBox(height: 50),
-            Text('Welcome back, you\'ve been missed!',
-                style: TextStyle(color: Colors.grey[700], fontSize: 16)),
-            const SizedBox(height: 25),
-            TextInput(
-                controller: usernameController,
-                hintText: 'Username',
-                obscureText: false),
-            const SizedBox(height: 10),
-            TextInput(
-                controller: passwordController,
-                hintText: 'Password',
-                obscureText: true),
-            const SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Text('Forgot Password?',
-                      style: TextStyle(color: Colors.grey[600])),
-                ],
+        child: SingleChildScrollView(
+          child: Center(
+            child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+              const SizedBox(height: 50),
+              const Icon(Icons.lock, size: 100),
+              const SizedBox(height: 50),
+              Text('Welcome back, you\'ve been missed!',
+                  style: TextStyle(color: Colors.grey[700], fontSize: 16)),
+              const SizedBox(height: 25),
+              TextInput(
+                  controller: usernameController,
+                  hintText: 'Username',
+                  obscureText: false),
+              const SizedBox(height: 10),
+              TextInput(
+                  controller: passwordController,
+                  hintText: 'Password',
+                  obscureText: true),
+              const SizedBox(height: 10),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text('Forgot Password?',
+                        style: TextStyle(color: Colors.grey[600])),
+                  ],
+                ),
               ),
-            ),
-            const SizedBox(height: 25),
-            PrimaryButton(onTap: signUserIn),
-            const SizedBox(height: 50),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Row(
-                children: [
-                  Expanded(
-                      child: Divider(
-                    thickness: 0.5,
-                    color: Colors.grey[400],
-                  )),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                    child: Text(
-                      'Or continue with',
-                      style: TextStyle(color: Colors.grey[700]),
+              const SizedBox(height: 25),
+              PrimaryButton(onTap: signUserIn),
+              const SizedBox(height: 50),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    )),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                      child: Text(
+                        'Or continue with',
+                        style: TextStyle(color: Colors.grey[700]),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                      child: Divider(
-                    thickness: 0.5,
-                    color: Colors.grey[400],
-                  ))
+                    Expanded(
+                        child: Divider(
+                      thickness: 0.5,
+                      color: Colors.grey[400],
+                    ))
+                  ],
+                ),
+              ),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: const [
+                  SSOTile(imagePath: 'lib/images/google.png'),
+                  SizedBox(width: 25),
+                  SSOTile(imagePath: 'lib/images/apple.png'),
                 ],
               ),
-            ),
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                SSOTile(imagePath: 'lib/images/google.png'),
-                SizedBox(width: 25),
-                SSOTile(imagePath: 'lib/images/apple.png'),
-              ],
-            ),
-            const SizedBox(height: 50),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('Not a member?',
-                    style: TextStyle(color: Colors.grey[700])),
-                const SizedBox(width: 5),
-                const Text('Register now',
-                    style: TextStyle(
-                        color: Colors.blue, fontWeight: FontWeight.bold)),
-              ],
-            )
-          ]),
+              const SizedBox(height: 50),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Not a member?',
+                      style: TextStyle(color: Colors.grey[700])),
+                  const SizedBox(width: 5),
+                  const Text('Register now',
+                      style: TextStyle(
+                          color: Colors.blue, fontWeight: FontWeight.bold)),
+                ],
+              )
+            ]),
+          ),
         ),
       ),
     );
