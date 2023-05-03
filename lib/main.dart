79ae532ffc38c2b0c3f6052b6ac55469bdebfb82
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-03 11:01:47 +0100

    feat: adding firebase imports to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:bubble_tea/models/shop.dart';
 import 'package:bubble_tea/pages/home_page.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(const MyApp());
