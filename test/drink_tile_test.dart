ccf82ab723d6f2895c3f5ed4ca95e720b2ea7004
Charlie Palmer <charlie.palmer@bluefrontier.co.uk>
2023-05-05 16:45:13 +0100

    feat: adding tests for drink tile widget

---
+++ test/drink_tile_test.dart
@@ -0,0 +1,32 @@
+import 'package:bubble_tea/components/drink_tile.dart';
+import 'package:bubble_tea/models/drink.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('DrinkTile', () {
+    testWidgets('displays drink name and image', (WidgetTester tester) async {
+      // Create a Drink object to use in the test
+      final drink = Drink(
+        name: 'Test Drink',
+        price: '\$3.50',
+        imagePath: 'lib/images/milk_tea.png',
+      );
+
+      // Create the DrinkTile widget
+      final widget = DrinkTile(
+        drink: drink,
+        onTap: () {},
+        trailing: const Icon(Icons.favorite_border),
+      );
+
+      // Add the widget to the widget tree
+      await tester.pumpWidget(MaterialApp(home: Scaffold(body: widget)));
+
+      // Verify that the drink name and image are displayed
+      expect(find.text('Test Drink'), findsOneWidget);
+      expect(find.text('\$3.50'), findsOneWidget);
+      expect(find.byType(Image), findsOneWidget);
+    });
+  });
+}
